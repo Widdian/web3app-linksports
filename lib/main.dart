75b4cacfc91e4e84a2b8e4a0af3e9f5195601acc
Roberto <roberto3154_421@hotmail.com>
2023-03-17 11:40:41 -0300

    feat: execute getit setup on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:web3app_linksports/setup.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await setup();
   runApp(const Web3App());
 }
